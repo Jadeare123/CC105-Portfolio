4e02f84c1536c723eede263e8696b21857d5ab83
Jadeare123 <72805555+Jadeare123@users.noreply.github.com>
2020-10-13 22:32:00 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sample JavaScript</title>
+	
+</head>
+<body>
+
+
+
+    	<h3> Whats up! Im Jade Bajenio Arellano </h3>
+
+    	<h2 id="Click">
+    		Touch me not
+    	</h2>
+
+<script type="text/javascript">
+	
+   <button type="button" onclick='document.getElementById("Click").innerHTML = "Whats up me amigoss"'> Click me
+   <button>
+
+</script>
+
+</body>
+</html>
